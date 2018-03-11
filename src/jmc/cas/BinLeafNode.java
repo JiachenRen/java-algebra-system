1bf40e3534e722bf029e097c16284d3068c2cde3
Jiachen Ren <jiachenren@greensboroday.org>
2018-03-10 23:33:42 -0500

    RawValue and Variable now extends from LeafNode, an abstract class that implements BinLeafNode and Operable

---
+++ src/jmc/cas/BinLeafNode.java
@@ -0,0 +1,8 @@
+package jmc.cas;
+
+/**
+ * Created by Jiachen on 3/4/18.
+ * BinLeafNode: classes that are not BinaryOperations. Just a tag.
+ */
+interface BinLeafNode {
+}
--- src/jmc/cas/LeafNode.java
+++ src/jmc/cas/LeafNode.java
@@ -1,8 +1,31 @@
 package jmc.cas;
 
 /**
- * Created by Jiachen on 3/4/18.
- * LeafNode: classes that are not BinaryOperations. Just a tag.
+ * Created by Jiachen on 3/10/18.
+ * Leaf Node: super class of Variable and RawValue
  */
-interface LeafNode {
+abstract class LeafNode implements BinLeafNode, Operable {
+    public abstract String toString();
+
+    public abstract boolean isUndefined();
+
+    public abstract Operable clone();
+
+    public abstract Operable explicitNegativeForm();
+
+    public int levelOf(Operable o) {
+        return this.equals(o) ? 0 : -1;
+    }
+
+    public int numNodes() {
+        return 1;
+    }
+
+    public Operable beautify() {
+        return this;
+    }
+
+    public Operable replace(Operable o, Operable r) {
+        return this.equals(o) ? r : this;
+    }
 }
