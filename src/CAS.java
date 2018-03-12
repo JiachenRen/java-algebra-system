b5d95721bbaa88a7d3854273ffecb97a05e01c7b
Jiachen Ren <arc4jiachenren@gmail.com>
2018-03-12 14:57:07 -0400

    Experimental JavaFX JMC CAS user interface.

--- src/CAS.java
+++ src/CAS.java
@@ -1,16 +1,21 @@
 
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import jmc.cas.Expression;
+import jmc.cas.Operable;
+import jmc.cas.Operation;
 
 /**
  * Created by Jiachen on 3/2/18.
  * JMC Computer Algebra System user interface model
  */
 public class CAS extends Application {
+    private SimpleStringProperty expression;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,9 +23,22 @@ public class CAS extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("cas.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("cas.fxml"));
+        Parent root = fxmlLoader.load();
+        CasController controller = fxmlLoader.getController();
+
+        expression = new SimpleStringProperty("");
+        expression.bind(controller.input.textProperty());
+        expression.addListener((observable, oldValue, newValue) -> {
+            System.out.println(oldValue + " " + newValue);
+            Operable operable = Expression.interpret(newValue);
+            controller.addition.setText(operable instanceof Operation ? ((Operation) operable).copy().toAdditionOnly().toString() : operable.toString());
+            controller.exponential.setText(operable instanceof Operation ? ((Operation) operable).copy().toExponentialForm().toString() : operable.toString());
+            controller.simplified.setText(operable.copy().simplify().toString()); //should conform to MVC, setText should be in controller.
+            controller.beautified.setText(operable.copy().simplify().beautify().toString());
+        });
         primaryStage.setTitle("JMC Computer Algebra System");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
 }
--- src/CasController.java
+++ src/CasController.java
@@ -1,6 +1,27 @@
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 /**
  * Created by Jiachen on 3/2/18.
  * CAS Controller
  */
 public class CasController {
+
+
+    public Label simplified;
+    public TextField input;
+    public Label beautified;
+    public Label addition;
+    public Label exponential;
+
+    public void textFieldInput(KeyEvent keyEvent) {
+        System.out.println(keyEvent.getEventType());
+        simplified.setVisible(true);
+        beautified.setVisible(true);
+        addition.setVisible(true);
+        exponential.setVisible(true);
+        System.out.println(input.textProperty().get());
+        System.out.println(input.textProperty().get());
+    }
 }
