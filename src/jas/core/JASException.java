5acd0ebac0753fd456621f6a8782a988686cf216
Jiachen Ren <arc4jiachenren@gmail.com>
2018-03-26 23:49:13 -0400

    Add JASException

---
+++ src/jas/core/JASException.java
@@ -0,0 +1,11 @@
+package jas.core;
+
+/**
+ * Created by Jiachen on 3/10/18.
+ * JMC Exception
+ */
+public class JASException extends RuntimeException {
+    public JASException(String msg) {
+        super(msg);
+    }
+}
